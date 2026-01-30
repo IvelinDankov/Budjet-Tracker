b7d957e5beb143ccda0073a98737dac9a7153df8
Velin Dankov <velin80@web.de>
2026-01-30 12:01:25 +0200

    Making service ready and dashboar some features.

--- src/app/dashboard/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard/dashboard.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { ExpenseService } from '../expense.service.js';
+import { ExpenseForm } from '../expense-form/expense-form';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CurrencyPipe, ExpenseForm],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
-export class Dashboard {
+export class Dashboard implements OnInit {
+  private expenseService = inject(ExpenseService);
 
+  expenses = this.expenseService.allExpenses;
+
+  balance = this.expenseService.balance;
+  totalIncome = this.expenseService.totalIncome;
+  totalOutcome = this.expenseService.totalExpenses;
+
+  ngOnInit(): void {
+    this.expenseService.load();
+  }
 }
